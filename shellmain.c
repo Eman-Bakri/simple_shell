0ca48222e345740de31b5244d59d33e2019b1256
Eman-Bakri <eman1996-sy@hotmail.com>
2023-08-22 20:07:50 -0700

    the main function of the program

---
+++ shellmain.c
@@ -0,0 +1,17 @@
+#include "shellheaders.h"
+
+/**
+ * main - main shell program
+ * @ac: argument count
+ * @av: argument value
+ * @env: environment variable
+ *
+ * Return - 0
+*/
+int main(int ac, char **av, char **env)
+{
+	if (ac == 1)
+		prompt_display(av, env);
+	return (0);
+}
+
