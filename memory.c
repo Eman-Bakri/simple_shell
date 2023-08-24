4382560272861bad395abbe7544ff5b38848c9fc
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-08-23 19:43:08 -0700

    fixing betty

--- memory.c
+++ memory.c
@@ -20,22 +20,3 @@ void mem_env(void)
 	free(environ);
 }
 
-/**
- * mem_args - Frees the args memory
- *
- * @args: pointer cto arguments.
- *
- * @begin: pointer to the beginning of args.
- */
-void mem_args(char **args, char **begin)
-{
-	size_t y;
-
-	for (y = 0; args[y] || args[y + 1]; y++)
-	{
-	/*free memory allocated for the current argument*/
-		free(args[y]);
-	}
-	/* free memory allocated for the array of arguments*/
-	free(begin);
-}
