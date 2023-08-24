afa6fbae1736ab826a1749b8157640d029d8d913
Eman-Bakri <eman1996-sy@hotmail.com>
2023-08-24 12:05:43 -0700

    free

--- ourgetline.c
+++ ourgetline.c
@@ -44,7 +44,6 @@ ssize_t our_getline(char **lineptr, size_t *num, FILE *stream)
 		/* if the buffer is full realloc the buffer to be one byte larger */
 		if (buff_read >= 127)
 			buffer = realloc(buffer, buff_read + 1);
-
 		buffer[buff_read] = curr_char;
 		buff_read++;
 	}
@@ -56,5 +55,6 @@ ssize_t our_getline(char **lineptr, size_t *num, FILE *stream)
 		buff_read = 0;
 	_comnthandle(buffer);
 	return (line_len);
+	free(buffer);
 }
 
