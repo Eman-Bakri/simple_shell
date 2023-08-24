640fb30aab9b463671c0ff6bbbc24621e4ea235c
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-08-23 19:44:11 -0700

    adding comments

--- memory.c
+++ memory.c
@@ -13,7 +13,7 @@ void mem_env(void)
 
 	for (x = 0; environ[x]; x++)
 	{
-	/*free memory allocated for the current environment variable*/
+	/*free the memory allocated for the current environment variable*/
 		free(environ[x]);
 	}
 	/* free memory allocated for the array of environment variables*/
