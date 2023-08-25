8f2b70536c1b90f677bddec33253106c5b67f469
Eman-Bakri <eman1996-sy@hotmail.com>
2023-08-25 15:03:11 -0700

    testig exec

--- displayprompt.c
+++ displayprompt.c
@@ -31,25 +31,31 @@ void prompt_display(char **av, char **env)
 			_printenv();
 			continue;
 		}
-		chpid = fork();
-		switch (chpid)
-		{	case -1:free(str);
-				exit(EXIT_FAILURE);
-				break;
-			case 0:
-				cmd = _cmdhandle(argv[0]);
-				if (cmd)
-					execve(cmd, argv, env);
-				if (execve(argv[0], argv, env) == -1)
-					printf("%s: %d: %s: not found\n", av[0], line_count, str);
-				exit(0);
-				free(cmd);
-				break;
-			default:
-				wait(&wtstatus);
-				if (wtstatus != 0)
-					exit(127);
-				break;
+		if (!str)
+			perror(av[0]);
+		else
+		{
+			chpid = fork();
+			switch (chpid)
+			{	case -1:
+					free(str);
+					exit(EXIT_FAILURE);
+					break;
+				case 0:
+					cmd = _cmdhandle(argv[0]);
+					if (cmd)
+						execve(cmd, argv, env);
+					if (execve(argv[0], argv, env) == -1)
+						printf("%s: %d: %s: not found\n", av[0], line_count, str);
+					exit(0);
+					free(cmd);
+					break;
+				default:
+					wait(&wtstatus);
+					if (wtstatus != 0)
+						exit(127);
+					break;
+			}
 		}
 	}
 }
