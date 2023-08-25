6a08756f96167280a9c69c178de10cf53a2cd488
Eman-Bakri <eman1996-sy@hotmail.com>
2023-08-25 12:22:06 -0700

    getline

--- displayprompt.c
+++ displayprompt.c
@@ -65,7 +65,7 @@ char *got_command(char *cmd)
 	ssize_t charCount;
 	int in;
 
-	charCount = getline(&str, &tu, stdin);
+	charCount = our_getline(&str, &tu, stdin);
 	/*Handle EOF*/
 	if (charCount == -1 || charCount == EOF)
 	{
@@ -92,6 +92,7 @@ void print_prompt(void)
 	if (isatty(STDIN_FILENO)) /*for interact/non interact mode check*/
 	{
 		write(1, "($) ", 4);
+		fflush(stdout);
 	}
 }
 /**
