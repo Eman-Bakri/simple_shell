5bb5c6031fc1ee96fb315dd9f851987923574c6b
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-08-25 18:09:44 -0700

    adding cd

---
+++ owncd.c
@@ -0,0 +1,39 @@
+#include "shellheaders.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * own_cd - Changes the current directory
+ *
+ * @cd_arg: The cd argument
+ *
+ * Return: Always 0 (Success)
+ */
+
+int own_cd(char *cd_arg)
+{
+	__attribute__((unused)) char *curr_dir = getcwd(NULL, 0);
+	char *home_dir = getenv("HOME");
+	char *oldpath = getenv("OLDPWD");
+
+	/*if argument is NULL or ~ return to home directory*/
+	if (cd_arg == NULL || strcmp(cd_arg, "~") == 0)
+	{
+		return (chdir(home_dir));
+	}
+	/*if argument is ..*/
+	else if (strcmp(cd_arg, "..") == 0)
+	{
+		return (chdir(".."));
+	}
+	/*if argument is - return t*/
+	else if (strcmp(cd_arg, "-") == 0)
+	{
+		return (chdir(oldpath));
+	}
+	else
+	{
+		return (chdir(cd_arg));
+	}
+}
